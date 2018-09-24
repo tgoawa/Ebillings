32f9345ef376eb261248b3976192537afdb3f1f9
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-09-24 13:19:47 -0500

    prepare component for unit tests

--- src/app/main-billing-processor/main-billing-processor.component.ts
+++ src/app/main-billing-processor/main-billing-processor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ICount } from 'src/app/model/count';
+import { DisplayCount, ICount } from 'src/app/model/count';
 import { BillingsProcessService } from '../service/billings-process.service';
+import { IBillingAccount } from '../model/billingAccount';
 
 @Component({
   selector: 'app-main-billing-processor',
@@ -8,31 +9,39 @@ import { BillingsProcessService } from '../service/billings-process.service';
   styleUrls: ['./main-billing-processor.component.css']
 })
 export class MainBillingProcessorComponent implements OnInit {
-  numberOfBillings: ICount;
-  countErrorMessage: string;
-  constructor(private billingService: BillingsProcessService) { }
+  displayCount: DisplayCount;
+
+  constructor(private billingsService: BillingsProcessService) { }
 
   ngOnInit() {
     this.getNumberOfBillings();
   }
 
   getNumberOfBillings() {
-    this.billingService.getBillingsCount()
+    this.billingsService.getBillingsCount()
     .subscribe((data: ICount) => {
       if (data) {
-        this.numberOfBillings = data;
+        this.displayCount = new DisplayCount(data.Count, 1);
       } else {
         console.error('No Billings Count object returned from service');
-        this.countErrorMessage = 'There was a problem retrieving the number of billings needed to process';
+        this.displayCount = new DisplayCount(data.Count, 2);
       }
     }, error => {
       console.error(error);
-      this.countErrorMessage = 'There was an error making the http request for count data';
+      this.displayCount = new DisplayCount(0, 3);
     });
   }
 
   processBillings() {
-    this.billingService.processBillings();
+    this.billingsService.processBillings()
+    .subscribe((data: IBillingAccount[]) => {
+      if (data.length === 0) {
+        console.log('All billings processed with no issues.');
+        // display message to view
+      } else {
+        // create list of billings with error for view.
+      }
+    });
   }
 
 }
--- src/app/model/count.ts
+++ src/app/model/count.ts
@@ -1,3 +1,13 @@
 export interface ICount {
   Count: number;
 }
+
+export class DisplayCount implements ICount {
+  Count = 0;
+  MessageType: number;
+
+  constructor(count: number, messageVal: number) {
+    this.Count = count;
+    this.MessageType = messageVal;
+  }
+}
