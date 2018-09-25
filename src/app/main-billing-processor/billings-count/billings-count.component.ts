82621c3825c40d470be26797d074240324790cd8
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-09-25 09:45:15 -0500

    fix issue with incorrect EventEmitter library imported into component

--- src/app/main-billing-processor/billings-count/billings-count.component.ts
+++ src/app/main-billing-processor/billings-count/billings-count.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ICount } from 'src/app/model/count';
-import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-billings-count',
