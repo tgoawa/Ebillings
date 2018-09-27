c6e6f2a3ad5e7e6203e1edeaf1d2de772cf049fa
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-09-27 13:31:29 -0500

    create function to process response from email service call

--- src/app/main-billing-processor/billing-account-processor/billing-account-processor.component.ts
+++ src/app/main-billing-processor/billing-account-processor/billing-account-processor.component.ts
@@ -16,9 +16,37 @@ export class BillingAccountProcessorComponent implements OnInit {
   updateBadEmail(account: IBillingAccount) {
     this.billingsService.updateBadEmail(account)
     .subscribe((data: IBillingAccount) => {
+      if (data) {
       // if data is empty object processes successfully and remove object from list
+      this.processUpdateEmailResponse(account, data);
+      } else {
+        // no object returned from service. This is an error
+        console.error('There was no data object returned from UpdateBadEmail() service call.');
+      }
     }, error => {
       console.error(error);
+      // an error occured display message to user
     });
+  }
+
+  private processUpdateEmailResponse(originalAccount: IBillingAccount,  responseAccount: IBillingAccount) {
+    const indexOfOriginalAccount = this.billingList.findIndex(account => account.tblDraftBillId === originalAccount.tblDraftBillId);
+    if (this.isEmpty(responseAccount)) {
+      if (indexOfOriginalAccount > 0) {
+        this.billingList.splice(indexOfOriginalAccount, 1);
+      }
+    } else {
+      // response had an object. Account was not processed. Display message to user
+      this.billingList[indexOfOriginalAccount] = responseAccount;
+    }
+  }
+
+  private isEmpty(obj) {
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        return false;
+      }
+    }
+    return true;
   }
 }
