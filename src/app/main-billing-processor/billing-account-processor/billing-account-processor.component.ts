c8af1986e4c51782c705d256f82688821972c65b
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-09-27 09:12:31 -0500

    implement billing account processor functionality

--- src/app/main-billing-processor/billing-account-processor/billing-account-processor.component.ts
+++ src/app/main-billing-processor/billing-account-processor/billing-account-processor.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IBillingAccount } from '../../model/billingAccount';
 
 @Component({
   selector: 'app-billing-account-processor',
   templateUrl: './billing-account-processor.component.html',
-  styleUrls: ['./billing-account-processor.component.css']
+  styleUrls: ['./billing-account-processor.component.css'],
 })
 export class BillingAccountProcessorComponent implements OnInit {
+  @Input() billingList: IBillingAccount[];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
